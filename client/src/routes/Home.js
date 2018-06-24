364f4651fb8e755e9a1d95c76d16c3a1ce42c6a7
Thomas Patricio Machado <thomaspmach@gmail.com>
2018-06-24 10:54:47 -0700

    starting UI

--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -1,7 +1,55 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import { Button } from 'semantic-ui-react';
+import { Grid } from 'semantic-ui-react';
+import { Image, List } from 'semantic-ui-react'
+//import image from './images/bill-melinda-gates.jpeg';
 
 export default () => (
+	<div>	
+		<Grid.Row>
+			<Image className="teste"src='/images/bill-melinda-gates.jpg' size='massive' />
+    	<h1>Bill & Melinda Gates Foundation</h1>
+    </Grid.Row>
+		<Grid centered columns={2}>
+	    <Grid.Column>
+	    	<List divided verticalAlign='middle'>
+			    <List.Item>
+			      <List.Content floated='right'>
+			        <Button>Donate</Button>
+			      </List.Content>
+			      <Image avatar src='/images/books.jpg' />
+			      <List.Content>Books</List.Content>
+			    </List.Item>
+			    <List.Item>
+			      <List.Content floated='right'>
+			        <Button>Donate</Button>
+			      </List.Content>
+			      <Image avatar src='/images/tables.jpeg' />
+			      <List.Content>Tables</List.Content>
+			    </List.Item>
+			    <List.Item>
+			      <List.Content floated='right'>
+			        <Button>Donate</Button>
+			      </List.Content>
+			      <Image avatar src='/images/paper.jpg' />
+			      <List.Content>Sheets</List.Content>
+			    </List.Item>
+			    <List.Item>
+			      <List.Content floated='right'>
+			        <Button>Donate</Button>
+			      </List.Content>
+			      <Image avatar src='/images/pens.jpeg' />
+			      <List.Content>Pens</List.Content>
+			    </List.Item>
+			  </List>
+	    </Grid.Column>
+	  </Grid>
+	</div>
+);
+
+
+/*
 	<div>	
 		<div>lala</div>
 		<ul>
@@ -13,4 +61,5 @@ export default () => (
 		  </li>
 		</ul>
 	</div>
-);
+*/
+
